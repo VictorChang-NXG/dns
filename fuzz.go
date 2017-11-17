22b45cb4eba9a67d3f8a0803cf6888a6dad71773
Miek Gieben <miek@miek.nl>
2017-11-17 11:06:56 +0000

    Add multiple fuzz function and adjust makefile

--- fuzz.go
+++ fuzz.go
@@ -3,18 +3,21 @@
 package dns
 
 func Fuzz(data []byte) int {
-	ret := 1
 	msg := new(Msg)
 
-	err := msg.Unpack(data)
-	if err != nil {
-		ret = 0
+	if err := msg.Unpack(data); err != nil {
+		return 0
+	}
+	if _, err := msg.Pack(); err != nil {
+		return 0
 	}
 
-	_, err = msg.Pack()
-	if err != nil {
-		ret = 0
-	}
+	return 1
+}
 
-	return ret
+func FuzzNewRR(data []byte) int {
+	if _, err := NewRR(string(data)); err != nil {
+		return 0
+	}
+	return 1
 }
